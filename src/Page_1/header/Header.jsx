e38c16e1b06e6ba9f1a871c64fda11cce404ecb1
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-12-11 11:25:31 +0100

    redirection logo vers page d'accueil

--- src/Page_1/header/Header.jsx
+++ src/Page_1/header/Header.jsx
@@ -1,22 +1,25 @@
-import React, { useContext } from 'react';
-import './header.css';
-import logo from './logo.svg';
-import { LanguageContext } from '../../Langue/LanguageContext.jsx';
+import React, { useContext } from 'react'
+import './header.css'
+import logo from './logo.svg'
+import { LanguageContext } from '../../Langue/LanguageContext.jsx'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
-  const { language, setLanguage } = useContext(LanguageContext);
+  const { language, setLanguage } = useContext(LanguageContext)
 
   return (
-      <div className="Header">
-        <img src={logo} alt="logo" className="logo" />
-        <select
-          className="language-select"
-          value={language}
-          onChange={(e) => setLanguage(e.target.value)}
-        >
-          <option value="en">EN</option>
-          <option value="fr">FR</option>
-        </select>
-      </div>
-  );
+    <div className="Header">
+      <Link to="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </Link>
+      <select
+        className="language-select"
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+      >
+        <option value="en">EN</option>
+        <option value="fr">FR</option>
+      </select>
+    </div>
+  )
 }
