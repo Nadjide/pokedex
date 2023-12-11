da05553d456681155f4c3715023f8ca7d7331f8e
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-12-11 16:24:07 +0100

    ajout du filtre par types avec les noms des types qui changent selon la langue

--- src/Page_1/PokemonList/Pokemonlist.jsx
+++ src/Page_1/PokemonList/Pokemonlist.jsx
@@ -4,11 +4,14 @@ import PokemonCard from '../PokemonCard/PokemonCard.jsx';
 import pokemonData from './pokemons.json';
 import SearchBar from '../SearchBar/SearchBar';
 import { LanguageContext } from '../../Langue/LanguageContext';
+import types from './types.json';
 
 export default function PokemonList() {
   const { language } = useContext(LanguageContext);
   const [searchTerm, setSearchTerm] = useState('');
   const [typeFilter, setTypeFilter] = useState('');
+
+  const uniqueTypes = Array.from(new Set(pokemonData.flatMap(pokemon => pokemon.types)));
 
   const filteredPokemons = pokemonData
     .filter(pokemon =>
@@ -18,23 +21,20 @@ export default function PokemonList() {
       return typeFilter ? pokemon.types.includes(typeFilter) : true;
     });
 
-    return (
-      <div className="pokemonlist">
-        <SearchBar setSearchTerm={setSearchTerm} />
-        <select className='typeSelect' value={typeFilter} onChange={e => setTypeFilter(e.target.value)}>
-          <option value="">Tous les types</option>
-          {pokemonData
-            .flatMap(pokemon => pokemon.types)
-            .filter((type, index, self) => self.indexOf(type) === index)
-            .map(type => (
-              <option key={type} value={type}>
-                {type}
-              </option>
-            ))}
-        </select>
-        {filteredPokemons.map(pokemon => (
-          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+  return (
+    <div className="pokemonlist">
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <select className='typeSelect' value={typeFilter} onChange={e => setTypeFilter(e.target.value)}>
+        <option value="">{types.normal.translations[language]}</option>
+        {uniqueTypes.map(type => (
+          <option key={type} value={type}>
+            {types[type].translations[language]}
+          </option>
         ))}
-      </div>
-    );
+      </select>
+      {filteredPokemons.map(pokemon => (
+        <PokemonCard key={pokemon.id} pokemon={pokemon} />
+      ))}
+    </div>
+  );
 }
